96217673af69d79c7e35352e58f0f4484b5c675c
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-03 17:48:04 -0500

    added shell of prompt in js

---
+++ script.js
@@ -0,0 +1,8 @@
+const userName = prompt("what's your name?");
+// const myName = "Patrick"; 
+
+<script>
+    if (userName == ""){prompt("what's your name?");
+    } else {document.write ("Hi, " + userName + ", welcome!" );
+    }
+</script>    
