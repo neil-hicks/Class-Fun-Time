b2f82a753678aed99dfe21c360b54e2d07720dd1
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-03 18:39:34 -0500

    completed js prompts

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const userName = prompt("what's your name?");
+const userName = prompt("What's your name?");
 const myName = "Patrick"; 
 
-if (userName == ""){prompt("what's your name?");
+if (userName == ""){prompt("What's your name?");
 } else if (userName == "Patrick") {alert ("Hi, " + userName + ", that's my name, too!");
 } else {
     alert ("Hi, " + userName + ", welcome!" );
+}
+
+const favoriteTrail = prompt("What's your trail to hike?"); 
+
+if (favoriteTrail == "JMT") {alert ("Mine, too!");
+} else {alert ("That's awesome!");
 }
