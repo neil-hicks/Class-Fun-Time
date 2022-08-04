6831b786e177f781e3fa3af44209e87410d1c905
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-04 14:40:08 -0500

    added function to js and coded out other items

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const userName = prompt("What's your name?");
+/*const userName = prompt("What's your name?");
 const myName = "Patrick"; 
 
 if (userName == ""){prompt("What's your name?");
 } else if (userName == "Patrick") {alert ("Hi, " + userName + ", that's my name, too!");
 } else {
     alert ("Hi, " + userName + ", welcome!" );
+} */
+
+
+function totalDays() {
+    const numTrips = prompt("How many hiking trips do you take a year?"); 
+    const averageDays = prompt("How many days does each trip usually last?");
+    let totalDays = numTrips * averageDays;
+    
+    alert("You hike " + totalDays + " days a year!");
+    
 }
 
+/* const hikingTrips = function(numTrips) {
+    if (numTrips >= 12) {
+        return "Wow, you're a pro!";
+    } else if (numTrips > 6 && numTrips < 11) {
+        return "Great job!";
+    } else {
+        return "You need to just go outside."
+    }
 const favoriteTrail = prompt("What's your trail to hike?"); 
 
 if (favoriteTrail == "JMT") {alert ("Mine, too!");
 } else {alert ("That's awesome!");
 }
+*/
